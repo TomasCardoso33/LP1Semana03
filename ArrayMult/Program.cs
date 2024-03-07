6ed92b0b4eebfcb6ec6c69cc6d18a0967c1871c0
Tomás <tomascardoso.dev@gmail.com>
2024-03-07 21:36:35 +0000

    Added first for cycle

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string s1 in args)
+            {
+                float f1 = float.Parse(s1);
+                Console.WriteLine(f1);
+            }
+        }
+    }
+}
