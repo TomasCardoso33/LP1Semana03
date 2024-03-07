1d7b127e7342faf46eb3aa7da3e2d5a1d1a61812
Tomás <tomascardoso.dev@gmail.com>
2024-03-07 20:58:33 +0000

    Rewrote most of the code again to use replace instead of a foreach cycle (seems to be working now)

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,25 +6,17 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Escreve qualquer coisa:  ");
-            string s1 = Console.ReadLine();
-            Console.WriteLine("Escreva uma letra");
-            string s2 = Console.ReadLine();
-            string s4 ="";
-            foreach(string s3 in s1)
-            {
-                s4 += s3;
-                if ( s3 == s2)
-                {
-                    s4+= "X";
-                }
-                else
-                {
-                    s4+=s3;
-                }
-                
-            }
-            Console.WriteLine(s4);
+            Console.WriteLine("Escreve algo: ");
+            string phrase = Console.ReadLine();
+
+            Console.WriteLine("Escreva uma letra: ");
+            string letra = Console.ReadLine();
+
+            string x = ("X");
+
+            phrase = phrase.Replace(letra, x);
+
+            Console.WriteLine("" + phrase);
             
         }
     }
