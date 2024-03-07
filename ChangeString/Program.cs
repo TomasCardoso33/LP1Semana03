1e807d23a86302ece5b52199e9b4b605285b986f
Tomás <tomascardoso.dev@gmail.com>
2024-03-07 20:30:19 +0000

    Added prompts for the user to write

---
+++ ChangeString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreve qualquer coisa:  ");
+            string s1 = ReadLine();
+            Console.WriteLine("Escreva uma letra");
+            string s2 = ReadLine();
+
+        }
+    }
+}
