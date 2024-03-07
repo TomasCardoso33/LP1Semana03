dc558965fd9cd86f80061f54981dd52d77a94760
Tomás <tomascardoso.dev@gmail.com>
2024-03-07 22:30:18 +0000

    Called the func

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+{
+    private static void PowersOf2Until5()
+    {
+        for (int i = 1; i <= (1 << 5); i = i << 1)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    static void Main()
+    {
+        PowersOf2Until5(); // Chamada do método PowersOf2Until5
+    }
+}
+}
