080bae0e4a49a344f271875b2060a85f5b8a84c0
Tomás <tomascardoso.dev@gmail.com>
2024-03-07 20:17:13 +0000

    Forgot to add the project

---
+++ RightSize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach ( string s1 in args)
+            {
+                if (s1.Length > 6)
+                {
+                    break;
+                }
+                
+                if (s1.Length > 3)
+                {
+                    Console.WriteLine(s1);
+                }
+                
+            }
+        }
+    }
+}
