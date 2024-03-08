980a96dd85b903edba968c07db268206ce644a9c
Tomás Cardoso <a22303462@alunos.ulht.pt>
2024-03-08 16:26:54 +0000

    First commit, user can input the ammount of players

---
+++ PlayerAchievements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PlayerAchievements
+{
+     [Flags]
+public enum Achievements
+{
+    None = 0,
+    DefeatOptionalBoss = 1,
+    FindHiddenLevel = 2,
+    FinishGame = 4
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha o número de jogadores: ");
+            string s1 = Console.ReadLine();
+            int n = int.Parse(s1);
+            Console.WriteLine(n);
+        }
+    }
+}
